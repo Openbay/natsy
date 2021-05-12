453c1b867ee7a3a3f60512cf19fcee077fbda8df
Keegan Leitz <kjleitz@gmail.com>
2021-05-12 13:42:58 -0400

    better logging, better error handling

--- lib/ruby_nest_nats.rb
+++ lib/ruby_nest_nats.rb
@@ -16,16 +16,16 @@ module RubyNestNats
         @logger
       end
 
-      def log(text)
-        logger.info("RubyNestNats | #{text}") if logger
+      def log(text, level: :info)
+        logger.send(level, "[#{Time.now}] RubyNestNats | #{text}") if logger
       end
 
-      def queue=(some_queue)
-        @queue = some_queue.to_s
+      def default_queue=(some_queue)
+        @default_queue = some_queue.to_s
       end
 
-      def queue
-        @queue
+      def default_queue
+        @default_queue
       end
 
       def replies
@@ -36,16 +36,13 @@ module RubyNestNats
         @started ||= false
       end
 
-      def reply_to(raw_subject, &block)
+      def reply_to(raw_subject, queue: self.default_queue, &block)
         subject = raw_subject.to_s
+        already_registered = replies.any? { |reply| reply[:subject] == subject }
 
-        if started?
-          raise StandardError, "NATS already started"
-        elsif !block_given?
-          raise ArgumentError, "Response block must be provided"
-        elsif replies.any? { |reply| reply[:subject] == subject }
-          raise ArgumentError, "Already registered a reply to #{subject}"
-        end
+        raise ArgumentError, "Already registered a reply to #{subject}" if already_registered
+        raise StandardError, "NATS already started" if started?
+        raise ArgumentError, "Response block must be provided" if !block_given?
 
         log("Registering a reply handler for subject '#{subject}'#{" in queue '#{queue}'" if queue}")
         replies << { subject: subject, handler: block, queue: queue }
@@ -55,10 +52,20 @@ module RubyNestNats
         NATS.start do
           replies.each do |replier|
             NATS.subscribe(replier[:subject], queue: replier[:queue]) do |message, inbox, subject|
-              log("Received the message '#{message}' for subject '#{subject}' with reply inbox '#{inbox}'")
-              response = replier[:handler].call(JSON.parse(message)["data"])
-              log("Responding with '#{response}'")
-              NATS.publish(inbox, response.to_json, queue: replier[:queue])
+              parsed_message = JSON.parse(message)
+              id, data = parsed_message.values_at("id", "data")
+
+              log("Received a message!")
+              log("  subject: #{subject}")
+              log("  data:    #{data.to_json}")
+              log("  id:      #{id}")
+              log("  inbox:   #{inbox}")
+
+              response_data = replier[:handler].call(data)
+
+              log("Responding with '#{response_data}'")
+
+              NATS.publish(inbox, response_data.to_json, queue: replier[:queue])
             end
           end
         end
@@ -66,18 +73,19 @@ module RubyNestNats
 
       def stop!
         log("Stopping NATS")
-        NATS.stop
+
+        NATS.stop rescue nil
         @started = false
       end
 
       def restart!
-        log("Restarting NATS...")
         stop!
         start!
       end
 
       def start!
         log("Starting NATS")
+
         return log("NATS is already running") if started?
 
         @started = true
@@ -86,8 +94,12 @@ module RubyNestNats
           Thread.handle_interrupt(StandardError => :never) do
             begin
               Thread.handle_interrupt(StandardError => :immediate) { listen }
-            ensure
+            rescue => e
+              log("Encountered an error: #{e.message} (#{e.class})", level: :error)
+              e.backtrace.each { |line| log("  #{line}", level: :error) }
+
               restart!
+              raise e
             end
           end
         end
